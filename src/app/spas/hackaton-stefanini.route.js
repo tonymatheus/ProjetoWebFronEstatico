97a92c991cca3deabf68274dffd5bb7e3b51b740
tonymatheus <tony.chaves14@gmail.com>
2020-03-18 17:08:18 -0300

    Iniciando Projeto_Front_End Angular

---
+++ src/app/spas/hackaton-stefanini.route.js
@@ -0,0 +1,45 @@
+(function(angular){
+    "use strict";
+
+    angular.module('hackaton-stefanini').config(function ($routeProvider) {
+        $routeProvider
+
+            /** Rota para Home */
+            .when('/', {
+                templateUrl: 'app/spas/homePage/template/home.tpl.html',
+                controller: 'HomeController as vm'
+            })
+            /** Rotas para Pessoas */
+            .when('/listarPessoas', {
+                templateUrl: 'app/spas/pessoas/template/pessoa-listar.tpl.html',
+                controller: 'PessoaListarController as vm'
+            })
+            .when('/EditarPessoas/:idPessoa', {
+                templateUrl: 'app/spas/pessoas/template/pessoa-incluir-alterar.tpl.html',
+                controller: 'PessoaIncluirAlterarController as vm'
+            })
+            .when('/cadastrarPessoa', {
+                templateUrl: 'app/spas/pessoas/template/pessoa-incluir-alterar.tpl.html',
+                controller: 'PessoaIncluirAlterarController as vm'
+            })
+            /** Rotas Para Perfils */
+            .when('/listarPerfis', {
+                templateUrl: 'app/spas/perfil/template/perfil-listar.tpl.html',
+                controller: 'PerfilListarController as vm'
+            })
+            .when('/EditarPerfis/:idPessoa', {
+                templateUrl: 'app/spas/perfil/template/pessoa-incluir-alterar.tpl.html',
+                controller: 'PerfilIncluirAlterarController as vm'
+            })
+            .when('/cadastrarPerfis', {
+                templateUrl: 'app/spas/perfil/template/perfil-incluir-alterar.tpl.html',
+                controller: 'PerfilIncluirAlterarController as vm'
+            })
+            
+            .otherwise({
+                templateUrl: 'index_ERROR.html'
+            });
+    });
+
+}(angular));
+
